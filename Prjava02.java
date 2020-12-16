b9afff762c02351563882ea5ee96d4dd407933a6
magani2021daw2 <magani2021daw2@protonmail.com>
2020-12-16 20:00:35 +0100

    Treballant amb ordres Git i branques: Versió 1.3 del projecte prjava02

--- Prjava02.java
+++ Prjava02.java
@@ -18,6 +18,8 @@ public class Prjava02 {
      */
     public static void main(String[] args) {
 		
+		System.out.println("Afegint codi a la branca01 del projecte prjava02");
+		
 		System.out.println("Afegint més codi a la branca00 del projecte prjava02");
 		
 		System.out.println("Creació d'una branca del projecte prjava02");
